31b544bc5cb304860d468c021e50e226bf7737ed
Jan Gołda <regzand@gmail.com>
2015-04-15 09:28:07 +0200

    Added mongoose-timestamp plugin

--- models/Post.js
+++ models/Post.js
@@ -6,8 +6,12 @@ var Post = database.Schema({
 	id:		{type: String, trim: true, lowercase: true},
 	title:		{type: String},
 	content:	{type: String},
-	author:		{type: String, ref: 'User'}
+	author:		{type: String, ref: 'User'},
+	createdAt:	{type: Date, default: Date.now}
 });
+
+// plugins
+Post.plugin(require('mongoose-timestamp'));
 
 // exports
 module.exports = database.model('Post', Post);
